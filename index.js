ef71cef85da12cb94d08baa1eb424fa45e3c0986
Moshe Karmel <moshekarmel1@gmail.com>
2017-04-07 12:59:51 -0400

    added TVP

--- index.js
+++ index.js
@@ -6,7 +6,9 @@ var TYPES = require('tedious').TYPES;
 
 module.exports = {
   exec: exec,
-  sproc: sproc
+  sproc: sproc,
+  tvp: tvp,
+  TYPES: TYPES
 };
 /**
  * Executes a sql query
@@ -92,6 +94,61 @@ function sproc(name, params, config, done) {
         request.addParameter(prop, determineType(params[prop]), params[prop]);
       }
     }
+
+    var result = [];
+    request.on('row', function(columns) {
+      var row = {};
+      columns.forEach(function(column) {
+        row[column.metadata.colName] = column.value;
+      });
+      result.push(row);
+    });
+    request.on('doneProc', function(rowCount, more) {
+      done(null, result);
+    });
+    connection.callProcedure(request);
+  });
+};
+
+/**
+ * Calls a sql stored proc
+ * @param {string} sproc -- stored procedure name string
+ * @param {string} tvpParamName -- parameter name
+ * @param {Object} tvpParams -- standard tedious table-value parameter object
+ * @param {Object} config -- standard tedious config object
+ * @param {Function} done -- standard node callback
+ */
+function tvp(name, tvpParamName, tvpParams, config, done) {
+  if(!name || (typeof name != 'string')){
+    throw new Error('Node-SQL: stored procedure name was not in the correct format.');
+    return;
+  }
+  if(!tvpParamName || (typeof tvpParamName != 'string')){
+    throw new Error('Node-SQL: stored procedure tvpParamName was not in the correct format.');
+    return;
+  }
+  if(!config || (typeof config != 'object')){
+    throw new Error('Node-SQL: config was not in the correct format.');
+    return;
+  }
+  if(!done || (typeof done != 'function')){
+    done = function(a, b){};
+  }
+  var connection = new Connection(config);
+  connection.on('connect', function(err) {
+    if(err){
+      done(err, null);
+      return;
+    }
+    var request = new Request(name, function(_err) {
+      if (_err) {
+        done(_err, null);
+        return;
+      }
+      connection.close();
+    });
+
+    request.addParameter(tvpParamName, TYPES.TVP, tvpParams);
 
     var result = [];
     request.on('row', function(columns) {
